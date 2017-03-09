d3e2bcc8871a8f13fb1e78bb7b777056e8248d95
Ram Goli <ramsgoli@gmail.com>
2017-03-09 13:23:15 -0800

    first commit

---
+++ project4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project4
+//
+//  Created by Ram Goli on 3/9/17.
+//  Copyright © 2017 Ram Goli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
