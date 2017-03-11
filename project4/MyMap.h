a96af0ecd5cb0282440c4feca4686cd7ad6a958f
Ram Goli <ramsgoli@gmail.com>
2017-03-11 14:37:03 -0800

    finished MyMap.h

--- project4/MyMap.h
+++ project4/MyMap.h
@@ -3,19 +3,61 @@
 // Skeleton for the MyMap class template.  You must implement the first six
 // member functions.
 
+
+
 template<typename KeyType, typename ValueType>
 class MyMap
 {
 public:
-	MyMap();
-	~MyMap();
-	void clear();
-	int size() const;
-	void associate(const KeyType& key, const ValueType& value);
+    MyMap() {
+        m_numNodes = 0;
+        head = nullptr;
+    }
+    
+	//~MyMap();
+	//void clear();
+    int size() const {
+        return m_numNodes;
+    }
+    void associate(const KeyType& key, const ValueType& value) {
+        if (head == nullptr) {
+            head = createNode(key, value);
+            m_numNodes++;
+            return;
+        }
+        node *cur = head;
+        for (;;) {
+            if (key == cur->key) {
+                head->value = value;
+                return;
+            }
+            if (key < cur->key) {
+                if (cur->left != nullptr)
+                    cur = cur->left;
+                else {
+                    cur->left = createNode(key, value);
+                    m_numNodes++;
+                    return;
+                }
+            } else if (key > cur->key) {
+                if (cur->right != nullptr)
+                    cur = cur->right;
+                else {
+                    cur->right = createNode(key, value);
+                    m_numNodes++;
+                    return;
+                }
+            }
+        }
+        
+    }
 
 	  // for a map that can't be modified, return a pointer to const ValueType
-	const ValueType* find(const KeyType& key) const;
-
+    const ValueType* find(const KeyType& key) const {
+        return searchFor(key, head);
+    }
+        
+     
 	  // for a modifiable map, return a pointer to modifiable ValueType
 	ValueType* find(const KeyType& key)
 	{
@@ -27,4 +69,40 @@ public:
 	MyMap& operator=(const MyMap&) = delete;
 
 private:
+    
+    struct node {
+        KeyType key;
+        ValueType value;
+        node *left;
+        node *right;
+    };
+    
+    node *head;
+    int m_numNodes;
+    
+    node* createNode(const KeyType& key, const ValueType& value) {
+        node *newNode = new node;
+        newNode->key = key;
+        newNode->value = value;
+        newNode->left = nullptr;
+        newNode->right = nullptr;
+        return newNode;
+    }
+                   
+    ValueType* searchFor(const KeyType& key, node *ptr) const {
+        
+        if (ptr == nullptr)
+            return nullptr;
+        
+        else if (key == ptr->key) {
+            return &ptr->value;
+        }
+        else if (key < ptr->key)
+            return searchFor(key, ptr->left);
+        else {
+            return searchFor(key, ptr->right);
+        }
+            
+    }
+    
 };
--- project4/main.cpp
+++ project4/main.cpp
@@ -1,7 +1,22 @@
 // The main.cpp you can use for testing will replace this file soon.
 
 #include "provided.h"
+#include "MyMap.h"
+#include <string>
+using namespace std;
+#include <iostream>
 
 int main()
 {
+    
+    MyMap<string,double> nameToGPA; // maps student name to GPA
+    // add new items to the binary search tree-based map
+    nameToGPA.associate("Carey", 3.5); // Carey has a 3.5 GPA
+    nameToGPA.associate("David", 3.99); // David beat Carey
+    nameToGPA.associate("Abe", 3.2); // Abe has a 3.2 GPA
+    
+    double* davidsGPA = nameToGPA.find("David");
+    if (davidsGPA != nullptr)
+        *davidsGPA = 1.5;
+    
 }
